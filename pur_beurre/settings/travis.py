1cd54491f55aa8af288c0350b18ec26380020bfb
flodelage <florian-delage@protonmail.com>
2021-12-23 17:50:10 +0000

    <first commit

---
+++ pur_beurre/settings/travis.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
