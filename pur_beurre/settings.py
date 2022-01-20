ea01c57aba429522aa8ae3262939d7b9f6d595b7
Florian Delage <delageflo@ymail.com>
2022-01-20 22:30:08 +0100

    fix collectstatic config issue

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
@@ -165,16 +165,16 @@ DOMAIN = os.environ.get('DOMAIN')
 
 
 # Sentry:
-# sentry_sdk.init(
-#     dsn="https://a56a7b252f1d45ccbf382695c8010a4a@o1097635.ingest.sentry.io/6119249",
-#     integrations=[DjangoIntegration()],
+sentry_sdk.init(
+    dsn="https://a56a7b252f1d45ccbf382695c8010a4a@o1097635.ingest.sentry.io/6119249",
+    integrations=[DjangoIntegration()],
 
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     # We recommend adjusting this value in production.
-#     traces_sample_rate=1.0,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
 
-#     # If you wish to associate users to errors (assuming you are using
-#     # django.contrib.auth) you may enable sending PII data.
-#     send_default_pii=True
-# )
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
