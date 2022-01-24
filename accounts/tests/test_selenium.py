e90ab2c55f3c9a4e8ce3024ab4c4591a6b20c21e
Florian Delage <delageflo@ymail.com>
2022-01-24 08:59:30 +0100

    add selenium test to home autocomplete

---
+++ accounts/tests/test_selenium.py
@@ -0,0 +1,27 @@
+
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import selenium.webdriver.support.ui as ui
+
+
+class AutocompleteTest(LiveServerTestCase):
+
+  def test_home_search(self):
+    driver = webdriver.Chrome('/Users/floriandelage/Downloads/chromedriver')
+    action = ActionChains(driver)
+    wait = ui.WebDriverWait(driver, 1000)
+    # wait.until(lambda driver: driver.find_element_by_tag_name("iframe").is_displayed())
+
+    driver.get('http://127.0.0.1:8000/')
+
+    #populate the input with data
+    autocomplete_input = driver.find_element_by_class_name('autocomplete-input')
+    autocomplete_input.send_keys('Nutella')
+
+    #click on the ul
+    ul = driver.find_element_by_id('autocomplete-result-list-1')
+    action.click(ul).perform()
+
+    assert 'Nutella' in driver.page_source
