c4cf9c8424e21d299b3842b83eaf62324c2b4ca0
Florian Delage <delageflo@ymail.com>
2022-01-27 15:39:03 +0100

    tweak test_selenium

--- accounts/tests/test_selenium.py
+++ accounts/tests/test_selenium.py
@@ -11,20 +11,18 @@ class HomeSearchTest(LiveServerTestCase):
     def test_home_autocomplete(self):
         driver = webdriver.Chrome('/Users/floriandelage/Downloads/chromedriver')
         action = ActionChains(driver)
-        wait = ui.WebDriverWait(driver, 1000)
-        # wait.until(lambda driver: driver.find_element_by_tag_name("iframe").is_displayed())
 
         driver.get('http://127.0.0.1:8000/')
 
         #populate the input with data
         autocomplete_input = driver.find_element_by_class_name('autocomplete-input')
         autocomplete_input.send_keys('Nutella')
-
+        action.pause(3)
         #click on the ul
         ul = driver.find_element_by_id('autocomplete-result-list-1')
         action.click(ul).perform()
 
-        assert 'Nutella' in driver.page_source
+        assert 'Nutella' in driver.page_source or 'nutella' in driver.page_source or 'NUTELLA' in driver.page_source
 
 
 class ResetPasswordTest(LiveServerTestCase):
@@ -32,14 +30,12 @@ class ResetPasswordTest(LiveServerTestCase):
     def test_reset_password(self):
         driver = webdriver.Chrome('/Users/floriandelage/Downloads/chromedriver')
         action = ActionChains(driver)
-        wait = ui.WebDriverWait(driver, 1000)
-        # wait.until(lambda driver: driver.find_element_by_tag_name("iframe").is_displayed())
 
         driver.get('http://127.0.0.1:8000/')
 
         login_a = driver.find_element_by_id('login_link')
         action.click(login_a).perform()
-        assert '/accounts/login/' in driver.current_url
+        assert '/accounts/login' in driver.current_url
 
         reset_a = driver.find_element_by_id('reset_link')
         action.click(reset_a).perform()
@@ -48,4 +44,19 @@ class ResetPasswordTest(LiveServerTestCase):
         email_input = driver.find_element_by_id('id_email')
         email_input.send_keys('test@django.com')
         email_input.send_keys(Keys.RETURN)
-        assert '/password_reset/done/' in driver.current_url
+        assert '/password_reset/done' in driver.current_url
+
+
+class AllProductsListTest(LiveServerTestCase):
+
+    def test_all_products_list_pagination(self):
+        driver = webdriver.Chrome('/Users/floriandelage/Downloads/chromedriver')
+        action = ActionChains(driver)
+
+        driver.get('http://127.0.0.1:8000/catalog/all_products/')
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        page_two = driver.find_element_by_id('page_2')
+        action.pause(3)
+        action.move_to_element(page_two).click().perform()
+        assert '/catalog/all_products/?page=2' in driver.current_url
